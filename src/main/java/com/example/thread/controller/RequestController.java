0c24170761f4b98aca20dba895a6850a0afd0492
Jwhyee <kawnsdud@naver.com>
2023-04-29 23:20:20 +0900

    refactor : thread memory func

--- src/main/java/com/example/thread/controller/RequestController.java
+++ src/main/java/com/example/thread/controller/RequestController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -19,14 +20,35 @@ public class RequestController {
 
     private final RequestService requestService;
 
+    private final Map<Long, Request> repo = new HashMap<>();
+    private Long requestId = 1L;
+
     @PostMapping("/")
+    public synchronized Request doMemoryRequest(RequestDto dto) {
+        Request newReq = Request.builder()
+                .id(requestId++)
+                .title(dto.getRequestTitle())
+                .savedDate(LocalDateTime.now())
+                .build();
+        repo.put(requestId, newReq);
+        log.info("request = {}", newReq);
+        return newReq;
+    }
+
+    @GetMapping("/data")
+    public Object showMemoryData() {
+        log.info("data_size={}", repo.size());
+        return repo;
+    }
+
+    //    @PostMapping("/")
     public Request doRequest(RequestDto dto) {
         Request request = requestService.saveRequest(dto);
         log.info("request = {}", request);
         return request;
     }
 
-    @GetMapping("/data")
+    //    @GetMapping("/data")
     public Object showData() {
         Map<Long, Request> requestMap = requestService.listToMap();
         log.info("data_size={}", requestMap.size());
